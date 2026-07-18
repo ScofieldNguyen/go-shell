5ec7d0d2fde8540dfdefbc574ff32b40f10878ec
scofieldnguyen <scofieldnguyen96@gmail.com>
2026-07-18 17:50:39 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,16 +5,54 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"slices"
+	"strconv"
 	"strings"
 )
 
-var builtinCommands = []string{"type", "exit", "echo"}
+var handlers map[string]func([]string)
+
+func handleEcho(params []string) {
+	fmt.Println(strings.Join(params[1:], " "))
+}
+
+func handleType(params []string) {
+	if len(params) > 1 {
+		checkCommand := params[1]
+		if handlers[checkCommand] != nil {
+			fmt.Printf("%s is a shell builtin\n", checkCommand)
+		} else {
+			path, err := exec.LookPath(checkCommand)
+			if err != nil {
+				fmt.Printf("%s: not found\n", checkCommand)
+			} else {
+				fmt.Printf("%s is %s\n", checkCommand, path)
+			}
+		}
+	} else {
+		fmt.Println("Please input a command")
+	}
+}
+
+func handleExit(params []string) {
+	if len(params) > 1 {
+		if i, err := strconv.Atoi(params[1]); err == nil {
+			os.Exit(i)
+		}
+	}
+	os.Exit(0)
+}
+
+func init() {
+	handlers = map[string]func([]string){
+		"echo": handleEcho,
+		"type": handleType,
+		"exit": handleExit,
+	}
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-ShellLoop:
 	for {
 		fmt.Print("$ ")
 		if scanner.Scan() {
@@ -22,35 +60,13 @@ ShellLoop:
 			params := strings.Fields(line)
 
 			if len(params) > 0 {
-				switch command := params[0]; command {
-				case "exit":
-					break ShellLoop
-				case "echo":
-					fmt.Println(strings.Join(params[1:], " "))
-				case "type":
-					if len(params) > 1 {
-						checkCommand := params[1]
-						if slices.Contains(builtinCommands, checkCommand) {
-							fmt.Printf("%s is a shell builtin\n", checkCommand)
-						} else {
-							path, err := exec.LookPath(checkCommand)
-							if err != nil {
-								fmt.Printf("%s: not found\n", checkCommand)
-							} else {
-								fmt.Printf("%s is %s\n", checkCommand, path)
-							}
-						}
-					} else {
-						fmt.Println("Please input a command")
-					}
-				default:
+				command := params[0]
+				if handler := handlers[command]; handler != nil {
+					handler(params)
+				} else {
 					fmt.Printf("%s: command not found\n", command)
 				}
 			}
 		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
 	}
 }
