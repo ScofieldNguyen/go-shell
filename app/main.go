737e2b9cf018db2d3b5f6ba9ccea610785f788c7
scofieldnguyen <scofieldnguyen96@gmail.com>
2026-07-18 15:57:35 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
 	fmt.Print("$ ")
+	if scanner.Scan() {
+		// Get command first
+		command := scanner.Text()
+
+		fmt.Printf("%s: command not found\n", command)
+	}
 }
