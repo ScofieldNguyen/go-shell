a9b2a8b5be52fc0f4ed095ca41ffcbd0943e9b39
scofieldnguyen <scofieldnguyen96@gmail.com>
2026-08-06 20:50:05 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 var builtinCommandHandlers map[string]func([]string)
@@ -20,14 +18,9 @@ func init() {
 }
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
 	for {
 		fmt.Print("$ ")
-		if scanner.Scan() {
-			line := scanner.Text()
-			params := strings.Fields(line)
-
+		if params, err := getParams(); err == nil {
 			if len(params) > 0 {
 				command := params[0]
 				if builtinHandler := builtinCommandHandlers[command]; builtinHandler != nil {
