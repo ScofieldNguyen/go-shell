2c970943801cdc941082f0875131303a96b05696
scofieldnguyen <scofieldnguyen96@gmail.com>
2026-09-10 22:04:30 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -23,13 +23,26 @@ func init() {
 // if not just returns regular os.stdout
 func parseRedirect(params []string) (io.Writer, io.Writer, []string) {
 	// has output redirect command
-	if redirectIndex := indexOfTarges(params, []string{">", "1>", "2>"}); redirectIndex != -1 {
+	if redirectIndex := indexOfTarges(params, []string{">", "1>", "2>", ">>"}); redirectIndex != -1 {
+		// create the file target
 		if redirectIndex+1 >= len(params) {
 			fmt.Fprintln(os.Stderr, "syntax error: no redirect target")
 			return os.Stdout, os.Stderr, params
 		}
+		filePath := params[redirectIndex+1]
 
-		file, err := os.Create(params[redirectIndex+1])
+		// open the file
+		var file *os.File
+		var err error
+
+		if indexOf(params, ">>") != -1 {
+			// create mode
+			file, err = os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		} else {
+			// append mode
+			file, err = os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		}
+
 		if err != nil {
 			fmt.Println(err)
 			return os.Stdout, os.Stderr, params
@@ -52,6 +65,7 @@ func main() {
 		if params, err := getParams(); err == nil {
 			if len(params) > 0 {
 				outWriter, errWriter, params := parseRedirect(params)
+
 				command := params[0]
 				if builtinHandler := builtinCommandHandlers[command]; builtinHandler != nil {
 					builtinHandler(params, outWriter, errWriter)
